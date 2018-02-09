f466ec940ef0142a398e0f7c2e81ad28048ade3b
macfisher <macfisher01@gmail.com>
2018-02-09 22:05:37 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # sorc-utils
 Sorc Lab network and utility scripts
+
+## SORC-JMPBX
+/usr/local/bin/netlist.php
+~/sorc-utils/SORC-JMPBX/network.csv
+alias netlist="/usr/local/bin/netlist.php"
---
+++ SORC-JMPBX/netlist.php
@@ -0,0 +1,43 @@
+<?php
+
+$network = getNetwork();
+printNetwork($network);
+
+function getNetwork()
+{
+	$contents = trim(file_get_contents("network.csv"));
+	$rows = explode("\n", $contents);
+
+	$network = array("ID" => "", "IP" => "");
+	$net_cnt = count($rows);
+
+	for ($i = 0; $i < $net_cnt; $i++) {
+		$row_expld = explode(",", $rows[$i]);
+		$network["ID"][$i] = $row_expld[0];
+		$network["IP"][$i] = $row_expld[1];
+	}
+
+	return $network;
+}
+
+function isOnline($ip)
+{
+	exec(sprintf('ping -c 1 -W 5 %s', escapeshellarg($ip)), $res, $rval);
+	return $rval === 0; // $rval = 0 if online
+}
+
+function printNetwork($network)
+{
+	$net_cnt = count($network);
+	for ($i = 0; $i < $net_cnt; $i++) {
+		$id = $network["ID"][$i];
+		$ip = $network["IP"][$i];
+		$status = (isOnline($ip)) ? "Online" : "Offline";
+
+		$content = $id . "\n";
+		$content .= "    " . $ip . " $status" . "\n\n";
+		echo $content;
+	}
+
+	return;
+}
